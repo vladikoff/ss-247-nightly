8703fdceff4fb657c374b6ebd5201b9007099957
Vlad Filippov <vlad.filippov@gmail.com>
2015-08-07 16:11:09 -0400

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+npm install
+npm test
+FIREFOX_BIN=/Applications/FirefoxNightly.app/Contents/MacOS/firefox-bin npm test 
+```
---
+++ index.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+var selenium = require('selenium-standalone');
+var webdriverio = require('webdriverio');
+var chalk = require('chalk');
+
+
+
+selenium.install({
+  // check for more recent versions of selenium here:
+  // http://selenium-release.storage.googleapis.com/index.html
+  version: '2.47.1',
+}, function() {
+
+  selenium.start(function(err, cp) {
+    if (err) throw err;
+
+
+    profile(function (profile) {
+      var opts = {
+        desiredCapabilities: {
+          //loggingPrefs: {"driver": "ALL", "server": "ALL", "browser": "ALL", "client": "ALL"},
+          browserName: 'firefox',
+          // set a custom profile
+          //firefox_profile: profile
+        }
+      };
+
+      // support $FIREFOX_BIN
+      if (process.env.FIREFOX_BIN) {
+        opts.desiredCapabilities.firefox_binary = process.env.FIREFOX_BIN;
+      }
+
+      setTimeout(function () {
+          webdriverio
+            .remote(opts)
+            .init()
+            .url('about:accounts')
+      }, 3000);
+    });
+
+    tailLogs();
+
+  });
+
+});
+
+
+var tailLogs = function() {
+
+  Tail = require('tail').Tail;
+
+  tail = new Tail('log-browser.log');
+
+  tail.on('line', function(data) {
+    if (data) {
+      var parsed = true;
+      try {
+        data = JSON.parse(data.toString());
+      } catch (e) {
+        parsed = false;
+        w(chalk.blue(data.toString()));
+      }
+
+      if (! parsed) {
+        return;
+      }
+
+      var msg = data.message.trim();
+      if (data.level === 'SEVERE') {
+        w(chalk.red(data.level + ':', msg));
+      } else if (data.level === 'WARNING') {
+        // WARNING log is ignored
+      } else if (msg.toLowerCase().indexOf('fxa') >= 0 || msg.toLowerCase().indexOf('account') >= 0 ) {
+        w(chalk.green(data.level + ':', msg));
+      } else {
+        w(chalk.blue(data.level + ':', msg));
+      }
+    }
+  });
+
+  tail.on("error", function(error) {
+    console.log('ERROR: ', error);
+  });
+
+};
+
+
+var w = function(s) {
+  console.log(s);
+};
